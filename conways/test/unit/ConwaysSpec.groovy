50eb5966a42b7c76293a55ba8f1309db769a2421
Bianca Leuca <bianca.leuca@gmail.com>
2017-01-18 10:29:03 +0200

    Dead cell with 0 neighbour(s) dies

--- conways/test/unit/ConwaysSpec.groovy
+++ conways/test/unit/ConwaysSpec.groovy
@@ -5,29 +5,31 @@ class ConwaysSpec extends Specification {
 
 
     @Unroll
-    def "alive cell with #numberOfNeighbours neighbour(s) #stateOfCell"() {
+    def "#initialStateOfCell cell with #numberOfNeighbours neighbour(s) #stateOfCell"() {
 
         when:
-        def cellIsAlive = tick(numberOfNeighbours)
+        def cellIsAlive = tick(numberOfNeighbours, cellIsInitallyAlive)
 
         then:
         expectedStateOfCell == cellIsAlive
 
         where:
-        numberOfNeighbours | expectedStateOfCell
-        0                  | false
-        1                  | false
-        2                  | true
-        3                  | true
-        4                  | false
+        cellIsInitallyAlive | numberOfNeighbours | expectedStateOfCell
+        true                | 0                  | false
+        true                | 1                  | false
+        true                | 2                  | true
+        true                | 3                  | true
+        true                | 4                  | false
+        false               | 0                  | false
 
         stateOfCell = expectedStateOfCell ? "lives" : "dies"
+        initialStateOfCell = cellIsInitallyAlive ? "alive" : "dead"
 
     }
 
 
-    def tick(numberOfNeighbours) {
-        if (numberOfNeighbours < 2 || numberOfNeighbours > 3)
+    def tick(numberOfNeighbours, cellIsInitiallyAlive) {
+        if (!cellIsInitiallyAlive || (numberOfNeighbours < 2 || numberOfNeighbours > 3))
             return false
         return true
     }
